9622fc16a715b5acec4a299702ea9e21613ef1f0
Jay Trivedi <36271236+jay4121997@users.noreply.github.com>
2022-04-08 15:57:04 -0400

    CK editor Updated, Image plugin working

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,31 @@
-import React from "react"
-import CKFullEditor from 'rc-ckfulleditor'
-import './App.css'
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import CloudinaryUploadImagePlugin from './upload'
+import './App.css';
 
 function App() {
-   const C_NAME = import.meta.env.VITE_YOUR_CLOUDINARY_NAME
-   const P_NAME = import.meta.env.VITE_YOUR_UNSIGNED_UPLOAD_PRESET_NAME
-   console.log(P_NAME);
-
+  
+  
   return (
-    <div className="App" style={{width:'800px',margin:'0 auto'}}>
-      <CKFullEditor
-      config={{
-        cloudinary: {
-          cloudName: C_NAME ,
-          uploadPreset: P_NAME
-        },
-        codeBlock: {
-          languages: [
-              { language: 'javascript', label: 'JavaScript' },
-              { language: 'css', label: 'CSS' },
-              { language: 'html', label: 'HTML' }
-          ]
-      },
-        toolbar:{
-          
-          items: [
-            'removeFormat',
-            '|',
-            'heading',
-            '|',
-            'fontSize',
-            'fontFamily',
-            'fontColor',
-            'fontBackgroundColor',
-            '|',
-            'bold',
-            'italic',
-            'underline',
-            'strikethrough',
-            'code',
-            'subscript',
-            'superscript',
-            '|',
-            'bulletedList',
-            'numberedList',
-            '|',
-            'alignment',
-            'indent',
-            'outdent',
-            '|',
-            'link',
-            'insertTable',
-            'specialCharacters',
-            'imageUpload',
-            'mediaEmbed',
-            '|',
-            'highlight',
-            'blockQuote',
-            'horizontalLine',
-            'pageBreak',
-            '|',
-            'restrictedEditingException',
-            '|',
-            'undo',
-            'redo',
-            'codeBlock',
-            'source'
-          ],
-          
-          shouldNotGroupWhenFull: true,
-          removeItems:['fontFamily',
-          'fontColor',
-          'fontBackgroundColor',
-          'pageBreak',
-          'highlight',
-          'restrictedEditingException',
-        ]
-        }
-      }}
-      onChange={(event, editor) => { 
-        const data = editor.getData()
-        console.log({ event, editor, data })
-      }}
-    />
-    </div> 
+    <div className="App">
+     <CKEditor
+                    editor={ Editor }
+                    onReady={ editor => CloudinaryUploadImagePlugin( editor )}
+                    config={ {
+                      toolbar:{
+                        shouldNotGroupWhenFull:true
+                      }
+                    }
+                  }
+                    
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onChange={ ( event, editor ) => {
+                    const data = editor.getData();
+                    console.log(  data );
+                } }
+                    
+                />
+    </div>
   )
 }
 
---
+++ src/upload.jsx
@@ -0,0 +1,15 @@
+import { CloudinaryUnsigned } from 'puff-puff/CKEditor'
+const C_NAME = import.meta.env.VITE_YOUR_CLOUDINARY_NAME
+   const P_NAME = import.meta.env.VITE_YOUR_UNSIGNED_UPLOAD_PRESET_NAME
+
+
+const CloudinaryUploadImagePlugin = editor => {
+  
+  editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+    return new CloudinaryUnsigned( loader, 
+      C_NAME, 
+      P_NAME, 
+      [ 160, 500, 1000, 1052 ]);
+  };
+}
+export default CloudinaryUploadImagePlugin
